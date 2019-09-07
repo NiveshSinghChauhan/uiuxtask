a15285644e7c7612d2cf60728f9292f682a1c5e8
NiveshSinghChauhan <nivesh1998@gmail.com>
2019-09-08 00:28:51 +0530

    COMMIT 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import dataJson from './data';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'uiuxtask';
+
+  data = dataJson;
+
 }
